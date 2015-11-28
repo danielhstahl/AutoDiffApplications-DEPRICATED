96c5b806446d44ade71a79863f08d4e1c0bdcdae
phillyfan1138 <danstahl1138@gmail.com>
2015-11-28 10:38:58 -0500

    made everything auto

--- BlackScholes.h
+++ BlackScholes.h
@@ -3,25 +3,33 @@
 #include <cmath>
 #include "AutoDiff.h"
 
-template<typename Underlying, typename Strike, typename Discount, typename BSSigma, typename OptionMaturity> /*every element is a candidate for sensitivities. */
-auto BSCall(Underlying &S0, Strike &k, Discount &discount, BSSigma &sigma, OptionMaturity &t){
+//template<typename Underlying, typename Strike, typename Discount, typename BSSigma, typename OptionMaturity> /*every element is a candidate for sensitivities. */
+auto BSCall(auto &S0, auto &k, auto &discount, auto &sigma, auto &t){
   /*if(t==0){
     if(S0>k){
       return S0-k;
     }
     else{
-      return S0-S0; //
+      return 0; //
     }
   }*/
   double s=sqrt(2.0);
-  OptionMaturity sqrtT=sqrt(t);
+  auto sqrtT=sqrt(t);
   auto d1=log(S0/(discount*k))/(sigma*sqrtT)+sigma*sqrtT*.5;
   return S0*(.5+.5*erf(d1/s))-k*discount*(.5+.5*(erf((d1-sigma*sqrtT)/s)));
 }
-template<typename Underlying, typename Strike, typename Discount, typename BSSigma, typename OptionMaturity> /*every element is a candidate for sensitivities.  */
-auto BSPut(Underlying S0, Strike k, Discount discount, BSSigma sigma, OptionMaturity t){
+//template<typename Underlying, typename Strike, typename Discount, typename BSSigma, typename OptionMaturity> /*every element is a candidate for sensitivities.  */
+auto BSPut(auto &S0, auto &k, auto &discount, auto &sigma, auto &t){
+  /*if(t==0){
+    if(S0<k){
+      return k-S0;
+    }
+    else{
+      return 0; //
+    }
+  }*/
   double s=sqrt(2.0);
-  OptionMaturity sqrtT=sqrt(t);
+  auto sqrtT=sqrt(t);
   auto d1=log(S0/(discount*k))/(sigma*sqrtT)+sigma*sqrtT*.5;
   return S0*(.5*erf(d1/s)-.5)+k*discount*(.5-.5*(erf((d1-sigma*sqrtT)/s)));
 }
